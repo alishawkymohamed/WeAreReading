2ecec3291457eb381cdb4df8483cd0c9d587b945
alishawkymohamed <ali.shawky.iti@gmail.com>
2020-05-18 00:29:43 +0200

    Frontend

--- WeAreReading/ClientApp/src/app/app-routing.module.ts
+++ WeAreReading/ClientApp/src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { SignupComponent } from "./auth/signup/signup.component";
+import { LoginComponent } from "./auth/login/login.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: SignupComponent,
+  },
+  {
+    path: "auth/signup",
+    component: SignupComponent,
+  },
+  {
+    path: "auth/login",
+    component: LoginComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
